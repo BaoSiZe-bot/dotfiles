c63a61f8f44ee214b832a698d5c24b43d375dfdf
abalone <noilinux@noilinux>
2025-02-28 17:32:41 +0800

    完善fish的依赖，添加vscode

--- apps/fish/config.fish
+++ apps/fish/config.fish
@@ -1,5 +1,21 @@
+# cargo install vivid fd-find bat du-dust yazi-fm yazi-cli cargo-update cargo-about eza bottom gitui tlrc zellij viu nu nufmt
+# cargo install fish --tag 4.0.0
+# cargo install --git https://github.com/d1y/scls
+# cargo install --git https://github.com/wlh320/rime-ls
+# fish_config theme save "Catppuccin Frappe"
+# BUG: async prompt has a bug when i switch directory
 set fish_greeting
 set -gx PATH ~/.bin ~/.local/bin ~/.cargo/bin $PATH
+set -gx LS_COLORS $(vivid generate catppuccin-frappe)
+set -gx MANPAGER "bat"
+# set -gx LESS_TERMCAP_mb '\e[1;31m'      # begin bold
+# set -gx LESS_TERMCAP_md '\e[1;34m'      # begin blink
+# set -gx LESS_TERMCAP_so '\e[01;45;37m'  # begin reverse video
+# set -gx LESS_TERMCAP_us '\e[01;36m'     # begin underline
+# set -gx LESS_TERMCAP_me '\e[0m'         # reset bold/blink
+# set -gx LESS_TERMCAP_se '\e[0m'         # reset reverse video
+# set -gx LESS_TERMCAP_ue '\e[0m'         # reset underline
+set -gx GROFF_NO_SGR 1                   # for konsole
 set -gx SHELL fish
 set -gx EDITOR nvim
 set -gx MYVIMDIR .vim
@@ -104,4 +120,4 @@ end
 starship init fish | source
 zoxide init fish | source
 fzy_key_bindings
-catnap
+# catnap
